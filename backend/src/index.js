d8e111c3828667fc4e79a2ec7616181a6aac254b
Andy210904 <adityadhane553@gmail.com>
2025-04-03 13:56:55 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,40 +1,37 @@
-const express = require("express")
+const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./lib/db.js");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const {app,server}  = require("./lib/socket.js")
-const authRoutes = require("./routes/auth.route.js")
-const messageRoutes = require("./routes/message.route.js")
 const path = require("path");
+const connectDB = require("./lib/db.js");
+const authRoutes = require("./routes/auth.route.js");
+const messageRoutes = require("./routes/message.route.js");
+const { app, server } = require("./lib/socket.js");
 
-
-app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-);
-dotenv.config()
-
+dotenv.config();
 const PORT = process.env.PORT;
-
+__dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
-
-  app.get("*" ,(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
   })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-server.listen(PORT, ()=>{
-    console.log("Server is running on port " + PORT);
-    connectDB();
-})
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
